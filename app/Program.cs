691ede464bf01ae70c3af7ac0491df8dc461d825
Daniel Reis <daniel.amorim@stone.com.br>
2021-02-18 12:57:40 -0300

    add services to startup

--- app/Program.cs
+++ app/Program.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Refit;
+using System;
+using System.Threading.Tasks;
 
 namespace MyStore.App
 {
@@ -17,7 +13,9 @@ namespace MyStore.App
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<ShoppingCart>();
+            builder.Services.AddRefitClient<IBackendClient>()
+                .ConfigureHttpClient(client => client.BaseAddress = new Uri("http://localhost:7070"));
 
             await builder.Build().RunAsync();
         }
