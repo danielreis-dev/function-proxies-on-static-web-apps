dce7925b8cf396b76e2b16b00f7254c63d44d2c0
Daniel Reis <daniel.amorim@stone.com.br>
2021-02-18 12:57:29 -0300

    implement shopping cart

---
+++ app/ShoppingCart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace MyStore.App
+{
+    public class ShoppingCart : INotifyPropertyChanged
+    {
+        public List<CatalogItem> Items { get; } = new();
+
+        public int Count => Items.Count;
+
+        public void AddItem(CatalogItem item)
+        {
+            Items.Add(item);
+            OnPropertyChanged(nameof(Count));
+        }
+
+        public void ClearCart()
+        {
+            Items.Clear();
+            OnPropertyChanged(nameof(Count));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public OrderItem[] CreateOrderItems() => Items.GroupBy(ci => new {ci.Id, ci.Price})
+            .Select(ci => new OrderItem(ci.Key.Id, ci.Count(), ci.Key.Price))
+            .ToArray();
+    }
+}
