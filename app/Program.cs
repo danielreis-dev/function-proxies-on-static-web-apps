20bec1bc13e18e49845c71ab00419cd561bbe60c
Daniel Reis <daniel.amorim@stone.com.br>
2021-02-18 14:29:20 -0300

    Revert "use top level for program"
    This reverts commit 6bc79441968ebc8d9e5cb7222225e0a5c44f9c9b.

--- app/Program.cs
+++ app/Program.cs
@@ -1,14 +1,23 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using MyStore.App;
 using Refit;
 using System;
+using System.Threading.Tasks;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+namespace MyStore.App
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
 
-builder.Services.AddSingleton<ShoppingCart>();
-builder.Services.AddRefitClient<IBackendClient>()
-    .ConfigureHttpClient(client => client.BaseAddress = new Uri("http://localhost:7070"));
+            builder.Services.AddSingleton<ShoppingCart>();
+            builder.Services.AddRefitClient<IBackendClient>()
+                .ConfigureHttpClient(client => client.BaseAddress = new Uri("http://localhost:7070"));
 
-await builder.Build().RunAsync();
+            await builder.Build().RunAsync();
+        }
+    }
+}
