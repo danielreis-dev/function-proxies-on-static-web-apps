29eb51c76c90169628e6140d568e650fd34486f8
Daniel Reis <daniel.amorim@stone.com.br>
2021-02-18 12:57:12 -0300

    implement IBackendClient with Refit

---
+++ app/IBackendClient.cs
@@ -0,0 +1,16 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace MyStore.App
+{
+    public interface IBackendClient
+    {
+        [Get("/api/catalog")]
+        Task<CatalogItem[]> GetCatalogAsync([Query] int index);
+
+        [Post("/api/orders")]
+        Task<OrderCreationResponse> CreateOrderAsync(OrderItem[] orderItems);
+    }
+
+    public record OrderCreationResponse(string OrderId, string Status);
+}
