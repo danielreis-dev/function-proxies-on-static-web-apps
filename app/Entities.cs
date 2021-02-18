2168835fd833b127085651b6f49c55e4b39d4523
Daniel Reis <daniel.amorim@stone.com.br>
2021-02-18 12:56:42 -0300

    Add entities class

---
+++ app/Entities.cs
@@ -0,0 +1,6 @@
+﻿namespace MyStore.App
+{
+    public record CatalogItem(int Id, string Name, string ImageUrl, decimal Price);
+
+    public record OrderItem(int Id, int Count, decimal IndividualPrice);
+}
